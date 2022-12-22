3bad17c5f0c4ada9ae8403b47d9d4a1749641e73
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-12-21 21:39:10 -0300

    construindo a logica do jogo

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,37 @@ function App() {
     [0, 0, 0, 0],
   ]);
 
+  const [isReveled, setIsReveled] = useState(
+    new Array(grid.length)
+      .fill("")
+      .map(() => new Array(grid[0].length).fill(false))
+  );
+
+  const handleSelectedCard = (row: number, column: number) => {
+    const newIsReveled = [...isReveled]
+    newIsReveled[row][column] = true;
+    setIsReveled(newIsReveled)
+    // número deve aparecer
+    // clicar novamente e um segundo número deve aparecer
+    // comparar os dois números:
+    // se for igual sussesso
+    // se for diferentes voltar os números ocutos
+    
+  };
+
   return (
     <div className="App">
       <div className="grid">
         {grid.map((row, rowIndex) => (
-          <div key={rowIndex}>
+          <div className="row" key={rowIndex}>
             {row.map((number, columnIndex) => (
-              <div key={columnIndex}>{number}</div>
+              <div
+                className="card"
+                key={columnIndex}
+                onClick={() => handleSelectedCard(rowIndex, columnIndex)}
+              >
+                {isReveled[rowIndex][columnIndex] ? number : ""}
+              </div>
             ))}
           </div>
         ))}
